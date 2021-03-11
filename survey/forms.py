7f3bc167fc6fd85f600facea511a04f85c3c30bd
rob70 <robholen@online.no>
2021-03-11 14:34:14 +0100

    Initial commit

--- survey/forms.py
+++ survey/forms.py
@@ -22,11 +22,36 @@ class OptionForm(forms.ModelForm):
 
 
 class AnswerForm(forms.Form):
+    """ I understand this as en override of the initialization method before 
+    the base method is called anew in super() """
+    # *args brukes for et ubestemt antall argumenter uten keyword
     def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items(): 
+            print ("%s : %s" %(key, value)) 
+        # options are removed from the keyword arguments
+        # and returned to the options variable
+        # Thus the intention is not to delete, but to manipulate.
+        # The data goes from a django QuerySet to python set
+        # Why? Set is an unordered list 
+
         options = kwargs.pop("options")
+        print("Type of options is: ", type(options))
+        # Django class "QuerySet"
+        print("Options are: ", options)
         # Options must be a list of Option objects
         choices = {(o.pk, o.text) for o in options}
+        print("Type of choices is: ", type(choices))
+        # returns class: 'set' Set is unordered? 
+        for a in choices:
+            print("elements in choices: ", a)
         super().__init__(*args, **kwargs)
+        print(" Have called super !")
+        if kwargs is not None:
+            print("---- These are kwargs, post super ---")
+            for key, value in kwargs.items(): 
+                print ("%s : %s" %(key, value))
+        else:
+            print("kwargs is None")
         option_field = forms.ChoiceField(choices=choices, widget=forms.RadioSelect, required=True)
         self.fields["option"] = option_field
 
--- survey/views/auth.py
+++ survey/views/auth.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.http.response import HttpResponseRedirect
