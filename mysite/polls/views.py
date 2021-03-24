9a30ec7e7624c5968bedb5c86878d3ae6819ce5a
1990youngsuk <63946899+1990youngsuk@users.noreply.github.com>
2021-03-24 17:48:37 +0900

    survey part3

---
+++ mysite/polls/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.template import loader
+
+# Create your views here.
+def index(request):
+    ''' template 과 loader 사용하는 방법
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(request, context))
+    '''
+    # 현재 TypeError: get() missing 1 required positional argument: 'header'
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    # return HttpResponse("You are looking at question %s." % question_id)
+    ''' using original tech of try render and else raise 404
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/details.html', {'question': question})
+    '''
+    # below uses get_object_or_404 shortcut API, which return 404 if data does not exist
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
