4b5aa1566f160a15e0d3950c84e01720464204f3
1990youngsuk <63946899+1990youngsuk@users.noreply.github.com>
2021-03-25 11:10:09 +0900

    survey original view version(before generic view)

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+'''
+# generic view version
+urlpatterns = [ 
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+'''
+
+# usual version
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 from django.template import loader
+from django.db.models import F
 
 # Create your views here.
+'''
 def index(request):
-    ''' template 과 loader 사용하는 방법
+    template 과 loader 사용하는 방법
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(request, context))
-    '''
+    
     # 현재 TypeError: get() missing 1 required positional argument: 'header'
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
         'latest_question_list': latest_question_list
     }
     return render(request, 'polls/index.html', context)
+    '''
 
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     # return HttpResponse("You are looking at question %s." % question_id)
@@ -35,15 +46,27 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice =  question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        # fixed the race condition using 'F' method and then refreshing
+        # F() objects assigned to model fields persist after saving the model instance and will be applied on each save(). For example:
+        # stories_filed will be updated twice in this case. If it’s initially 1, the final value will be 3. This persistence can be avoided by reloading the model object after saving it, for example, by using refresh_from_db().
+        selected_choice.votes = F('votes') + 1        
+        selected_choice.save()
+        selected_choice.refresh_from_db()    
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with Post data. This prevents data from being poseted twice if a
+        # user hits the Back button
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
