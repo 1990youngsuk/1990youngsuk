6dd19adbbd38ebd2a1c2c981e134e511aac5b042
1990youngsuk <63946899+1990youngsuk@users.noreply.github.com>
2021-03-26 16:40:07 +0900

    css and js amended

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,10 +1,41 @@
 from django.contrib import admin
 
+'''
+# Register your models here.
+from .models import Question, Choice Survey
+
+class ChoiceInLine(admin.TabularInline):    
+    model = Choice
+    extra = 3
+
+class QuestionInLine(admin.TabularInline):
+    model = Question
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date'] # side bar box
+    search_fields = ['question_text']   # search form box
+
+class SurveyAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['survey_title']}),
+        (None, {'fields': ['survey_description']}),
+    ]
+    inlines = [QuestionInLine]
+    list_display = ('survey_title','survey_description')
+
+
+admin.site.register(Survey, SurveyAdmin)
+'''
+
+from django.contrib import admin
+
 # Register your models here.
 from .models import Question, Choice
 
-class ChoiceInLine(admin.TabularInline):
-    #...
+class ChoiceInLine(admin.TabularInline):    
     model = Choice
     extra = 3
 
@@ -17,5 +48,6 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date'] # side bar box
     search_fields = ['question_text']   # search form box
+
 
 admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,16 +5,74 @@ from django.utils import timezone
 
 # Create your models here.
 
+'''
+class Survey(models.Model):
+    survey_title = models.CharField(max_length=100, default="Title")
+    survey_description = models.CharField(max_length=500, default="Description")
+    survey_reward = models.IntegerField(default=500)
+    survey_created_at = models.DateTimeField(auto_now_add=True, null=True)
+    survey_updated_at = models.DateTimeField(auto_now=True, null=True)
+    
+    def when_posted(self):
+        now = timezone.now()
+        return now
+    
+    def __str__(self):
+        return self.survey_title
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.survey_created_at <= now
+    was_published_recently.admin_order_field = 'survey_created_at'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+'''
+
+class Question(models.Model):
+    #survey = models.ForeignKey(Survey, on_delete=models.CASCADE)    
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
+'''
 class Survey(models.Model):
     survey_title = models.CharField(max_length=100, default="Title")
     survey_description = models.CharField(
         max_length=500, default="Description")
     survey_reward = models.IntegerField(default=500)
+    
+    def when_posted(self):
+        now = timezone.now()
+        return now
+    
+    def __str__(self):
+        return self.survey_title
 
-    def when_posted():
-        return timezone.now()
-
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Question(models.Model):
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
@@ -39,3 +97,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+'''
